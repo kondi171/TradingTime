0735f85949c8383f356b8e05ff37e70fa6d838ce
Kondi171 <wk.k.nowak@gmail.com>
2021-11-03 17:16:56 +0100

    Styling footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,25 @@
 function Footer() {
     return ( 
-        <footer>Trading Time - 2021 r</footer>
+        <footer>
+            <div className="team-contact">
+                <h4>Kontakt</h4>
+                <ul>
+                    <li>Kamil Wypych - <i>kwypych4@gmail.com</i></li>
+                    <li>Konrad Nowak - <i>wk.k.nowak@gmail.com</i></li>
+                    <li>Krystian Łabęcki - <i>krystian.l@gmail.com</i></li>
+                    <li>Agnieszka Rabiej - <i>a.rabiej@gmail.com</i></li>
+                </ul>
+            </div>
+            <div className="footer-wrapper">
+                <div className="privacy-policy">
+                    <h4>Polityka Prywatności</h4>
+                    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Esse hic vel itaque. Ipsam, sequi laudantium voluptatibus dolores unde vel facere magnam tenetur voluptas accusantium fugiat assumenda itaque iusto, error alias praesentium magni nihil suscipit optio nam delectus blanditiis. Recusandae, ex! Doloribus praesentium placeat nulla eligendi recusandae atque rem quos ullam?</p>
+                </div>
+                <div className="copyrights">
+                    Wszelkie prawa Zastrzeżone &copy; Trading Time - 2021r 
+                </div>
+            </div> 
+        </footer>
     );
 }
 // Obraz <a href="https://pixabay.com/pl/users/itneverends-6669706/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=2930704"> ItNeverEnds</a> z <a href="https://pixabay.com/pl/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=2930704"> Pixabay</a>
