58716deac3c3f5227461ab4c679e19b04d37c9dd
kwypych4 <kwypych4@gmail.com>
2021-11-22 20:06:46 +0100

    Changed structure of the project - React Router

--- src/components/landing page/Nav.js
+++ src/components/landing page/Nav.js
@@ -6,6 +6,33 @@ class Nav extends React.Component {
     hideNavPx: 0,
   };
 
+  scrollToSection = (target, duration) => {
+    let actualPosition = document.querySelector(target).offsetTop;
+    let windowHeight = document.documentElement.clientHeight;
+
+    let startPosition = window.pageYOffset;
+
+    let distance = actualPosition - startPosition - windowHeight / 20;
+    let startTime = null;
+
+    function animation(currentTime) {
+      if (startTime == null) startTime = currentTime;
+      let timeElapsed = currentTime - startTime;
+      let run = ease(timeElapsed, startPosition, distance, duration);
+      window.scrollTo(0, run);
+      if (timeElapsed < duration) requestAnimationFrame(animation);
+    }
+
+    function ease(t, b, c, d) {
+      t /= d / 2;
+      if (t < 1) return (c / 2) * t * t + b;
+      t--;
+      return (-c / 2) * (t * (t - 2) - 1) + b;
+    }
+
+    requestAnimationFrame(animation);
+  };
+
   takeScrollPosition = () => {
     window.addEventListener('scroll', (e) =>
       this.setState({ scrollPosition: window.scrollY })
@@ -45,8 +72,15 @@ class Nav extends React.Component {
   render() {
     return (
       <nav>
-        <a href='#about'>Kim jesteśmy?</a>
-        <a href='#how'>Jak to działa?</a>
+        <a href='#about' onClick={() => this.scrollToSection('.about', 1000)}>
+          Kim jesteśmy?
+        </a>
+        <a
+          href='#how'
+          onClick={() => this.scrollToSection('.how-it-works', 1000)}
+        >
+          Jak to działa?
+        </a>
         <img src='./resources/img/logo.jpg' alt='' />
         <a href='#'>Zaloguj się</a>
         <a href='#'>Zarejestruj się</a>
--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import LandingPage from './components/landing page/LandingPage';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <LandingPage />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
