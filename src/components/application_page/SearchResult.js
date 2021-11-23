7b5f61dd3157b78bfaf314c1b724a5fd43edbf66
kwypych4 <kwypych4@gmail.com>
2021-11-23 22:15:16 +0100

    SearchPage Update

--- src/components/application_page/SearchResult.js
+++ src/components/application_page/SearchResult.js
@@ -1,9 +1,10 @@
-import image from '../../resources/img/testimages/allegro-favicon.png';
+import allegro from '../../resources/img/testimages/allegro-favicon.png';
+import cdpsa from '../../resources/img/testimages/cdpsa-favicon.png';
 
-function SearchResult({ actionName }) {
+function SearchResult({ actionName, image, click }) {
   return (
     <>
-      <div className='search-page_result'>
+      <div className='search-page_result' onClick={click}>
         <img src={image} alt='' />
         <h3>{actionName}</h3>
       </div>
--- src/components/application_page/pages/SearchPage.js
+++ src/components/application_page/pages/SearchPage.js
@@ -1,8 +1,83 @@
 import React from 'react';
 import SearchResult from '../SearchResult';
+import allegro from '../../../resources/img/testimages/allegro-favicon.png';
+import cdpsa from '../../../resources/img/testimages/cdpsa-favicon.png';
+
 class SearchPage extends React.Component {
   state = {
     searchQuery: '',
+    actions: [
+      {
+        id: 0,
+        actionName: 'Allegro',
+        price: '4,20',
+        image: allegro,
+      },
+      {
+        id: 1,
+        actionName: 'CD Project Red',
+        price: '1,10',
+        image: cdpsa,
+      },
+    ],
+    activeAction: '',
+  };
+
+  handleChangeActiveAction = (id) => {
+    let currentId = this.state.activeAction;
+    let choosenId = id;
+    console.log('current id' + currentId);
+    console.log('choosen id' + id);
+
+    this.setState({ activeAction: id });
+
+    this.handleShowInfo();
+  };
+
+  actionList = [...this.state.actions].map((result) => (
+    <SearchResult
+      key={result.id}
+      actionName={result.actionName}
+      price={result.price}
+      image={result.image}
+      click={() => this.handleChangeActiveAction(result.id)}
+    />
+  ));
+
+  handleSearch = (e) => {
+    const searchQuery = e.target.value;
+    this.setState({
+      searchQuery,
+    });
+
+    this.showResults(searchQuery);
+  };
+
+  showResults = (searchQuery) => {
+    let results = [...this.state.actions];
+
+    results = results.filter((result) => {
+      return result.actionName
+        .toLowerCase()
+        .includes(searchQuery.toLowerCase());
+    });
+
+    this.actionList = results.map((result) => (
+      <SearchResult
+        key={result.id}
+        actionName={result.actionName}
+        price={result.price}
+        image={result.image}
+        click={() => this.handleChangeActiveAction(result.id)}
+      />
+    ));
+  };
+
+  handleShowInfo = () => {
+    document
+      .querySelector('.search-page_searcher')
+      .classList.toggle('minimized');
+    document.querySelector('.search-page_info').classList.toggle('active');
   };
 
   render() {
@@ -11,13 +86,15 @@ class SearchPage extends React.Component {
         <main className='search-page'>
           <div className='search-page_search-wrapper'>
             <div className='search-page_searcher'>
-              <input type='text' placeholder='Wyszukaj...' />
+              <input
+                type='text'
+                onChange={this.handleSearch}
+                value={this.state.searchQuery}
+                placeholder='Wyszukaj...'
+              />
             </div>
 
-            <div className='search-page_results'>
-              <SearchResult actionName='Allegro' />
-              <SearchResult actionName='CD Project Red' />
-            </div>
+            <div className='search-page_results'>{this.actionList}</div>
           </div>
 
           <div className='search-page_info'></div>
