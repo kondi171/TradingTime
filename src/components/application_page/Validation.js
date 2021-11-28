f4c43fe56f9fc0a417c644c2712d1392a7f6ac6a
kwypych4 <kwypych4@gmail.com>
2021-11-28 20:32:14 +0100

    Added Validation function

---
+++ src/components/application_page/Validation.js
@@ -0,0 +1,28 @@
+function Validation(type, value, confirmValue) {
+  let regex;
+  let isCorrect;
+
+  if (type === 'password') {
+    console.log(value);
+    console.log(confirmValue);
+    const regex = /(?=.*\w)(?=.*\W)(?=.*[A-Z]).{8,}/;
+    if (value === confirmValue) isCorrect = new RegExp(regex).test(value);
+    else isCorrect = 'DifferentValues';
+  }
+
+  if (type === 'email') {
+    regex =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    isCorrect = new RegExp(regex).test(value);
+  }
+
+  if (type === 'telephone') {
+    regex = /(?=.*[0-9])^.{9,9}$/;
+    isCorrect = new RegExp(regex).test(value);
+  }
+
+  console.log(isCorrect);
+  return isCorrect;
+}
+
+export default Validation;
