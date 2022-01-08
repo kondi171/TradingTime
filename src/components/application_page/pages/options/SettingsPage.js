a3788a3f776f9f5d3bf90b2f2d73a14fba51d09d
kwypych4 <kwypych4@gmail.com>
2022-01-08 22:19:05 +0100

    added admin authorisation in settings (displaying admin options)

--- src/components/application_page/pages/options/SettingsPage.js
+++ src/components/application_page/pages/options/SettingsPage.js
@@ -1,58 +1,60 @@
+import React, { useContext } from 'react';
 import { Outlet, NavLink } from 'react-router-dom';
 
+import { AppContext } from '../../../../AppContext';
+
 const SettingsPage = () => {
-  const isAdmin = true;
+  const { isUserAdmin } = useContext(AppContext);
+
   return (
-    <>
-      <main className='settings-page'>
-        <aside className='settings-page__categories'>
-          <h1 id='settings-theme'>Ustawienia</h1>
-          <ul>
-            <li className='aside-theme'>
-              <NavLink
-                className='settings-page__categories--navigation'
-                to='userpreferences'
-              >
-                <i className='fa fa-user' aria-hidden='true'></i>Ustawienia
-                użytkownika
-              </NavLink>
-            </li>
-            <li className='aside-theme'>
-              <NavLink
-                className='settings-page__categories--navigation'
-                to='applicationpreferences'
-              >
-                <i className='fa fa-cogs' aria-hidden='true'></i>
-                Ustawienia aplikacji
-              </NavLink>
-            </li>
-            <li className='aside-theme'>
-              <NavLink
-                className='settings-page__categories--navigation'
-                to='walletpreferences'
-              >
-                <i className='fa fa-credit-card-alt' aria-hidden='true'></i>
-                Ustawienia portfela
-              </NavLink>
-            </li>
+    <main className='settings-page'>
+      <aside className='settings-page__categories'>
+        <h1 id='settings-theme'>Ustawienia</h1>
+        <ul>
+          <li className='aside-theme'>
+            <NavLink
+              className='settings-page__categories--navigation'
+              to='userpreferences'
+            >
+              <i className='fa fa-user' aria-hidden='true'></i>Ustawienia
+              użytkownika
+            </NavLink>
+          </li>
+          <li className='aside-theme'>
+            <NavLink
+              className='settings-page__categories--navigation'
+              to='applicationpreferences'
+            >
+              <i className='fa fa-cogs' aria-hidden='true'></i>
+              Ustawienia aplikacji
+            </NavLink>
+          </li>
+          <li className='aside-theme'>
+            <NavLink
+              className='settings-page__categories--navigation'
+              to='walletpreferences'
+            >
+              <i className='fa fa-credit-card-alt' aria-hidden='true'></i>
+              Ustawienia portfela
+            </NavLink>
+          </li>
 
-            {isAdmin ? (
-              <li className='aside-theme'>
-                <NavLink
-                  className='settings-page__categories--navigation'
-                  to='adminoptions'
-                >
-                  <i className='fa fa-cog' aria-hidden='true'></i>
-                  Ustawienia administracyjne
-                </NavLink>
-              </li>
-            ) : null}
-          </ul>
-        </aside>
+          {isUserAdmin ? (
+            <li className='aside-theme'>
+              <NavLink
+                className='settings-page__categories--navigation'
+                to='adminoptions'
+              >
+                <i className='fa fa-cog' aria-hidden='true'></i>
+                Ustawienia administracyjne
+              </NavLink>
+            </li>
+          ) : null}
+        </ul>
+      </aside>
 
-        <Outlet />
-      </main>
-    </>
+      <Outlet />
+    </main>
   );
 };
 
