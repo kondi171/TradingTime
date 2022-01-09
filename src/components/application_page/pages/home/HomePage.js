de64dce863e4e2def4a86df985e496ef67bd1984
kwypych4 <kwypych4@gmail.com>
2022-01-09 22:52:29 +0100

    Added loading bar when all data is not loaded

--- src/components/application_page/pages/home/HomePage.js
+++ src/components/application_page/pages/home/HomePage.js
@@ -6,6 +6,7 @@ import HomePageDate from './HomePageDate';
 import RestrictedInfo from '../../../features/RestrictedInfo';
 
 import { AppContext } from '../../../../AppContext';
+import LoadingBar from '../../../features/LoadingBar';
 
 const HomePage = () => {
   // state = {
@@ -55,13 +56,19 @@ const HomePage = () => {
   const [restricted, setRestricted] = useState(false);
   const [favouriteActions, setFavouriteActions] = useState('');
   const [boughtActions, setBoughtActions] = useState('');
+  const [currentWallpaper, setCurrentWallpaper] = useState('');
+  const [currentTheme, setCurrentTheme] = useState('');
+  const [isLoaded, setIsLoaded] = useState(false);
 
   const { userFavouriteActions } = useContext(AppContext);
   const { userBoughtActions } = useContext(AppContext);
 
   const { userSettings } = useContext(AppContext);
-  const [currentWallpaper, setCurrentWallpaper] = useState();
-  useEffect(() => setWallpaper(), [userSettings]);
+
+  const checkIfDataIsLoaded = () => {
+    if (currentWallpaper !== '' && currentTheme !== '') setIsLoaded(true);
+  };
+
   const setWallpaper = () => {
     const home = document.getElementById('home');
     if (Number(userSettings.wallpaper) === 1) {
@@ -69,25 +76,20 @@ const HomePage = () => {
       home.classList.add('wallpaper1');
       home.classList.remove('wallpaper2');
       home.classList.remove('wallpaper3');
-    }
-    else if (Number(userSettings.wallpaper) === 2) {
+    } else if (Number(userSettings.wallpaper) === 2) {
       setCurrentWallpaper(2);
       home.classList.add('wallpaper2');
       home.classList.remove('wallpaper1');
       home.classList.remove('wallpaper3');
-    }
-    else {
+    } else {
       setCurrentWallpaper(3);
       home.classList.add('wallpaper3');
       home.classList.remove('wallpaper2');
       home.classList.remove('wallpaper1');
     }
-  }
+  };
 
-  const [currentTheme, setCurrentTheme] = useState();
-  useEffect(() => setTheme(), [userSettings]);
   const setTheme = () => {
-
     if (Number(userSettings.theme) === 1) {
       setCurrentTheme(1);
       document.documentElement.style.setProperty('--bg-color', '#333333');
@@ -95,16 +97,14 @@ const HomePage = () => {
       document.documentElement.style.setProperty('--text-color', '#c4c4c4');
       document.documentElement.style.setProperty('--active-color', '#3d84f5');
       document.documentElement.style.setProperty('--hover-color', '#555555');
-    }
-    else if (Number(userSettings.theme) === 2) {
+    } else if (Number(userSettings.theme) === 2) {
       setCurrentTheme(2);
       document.documentElement.style.setProperty('--bg-color', '#1F3336');
       document.documentElement.style.setProperty('--box-color', '#038C3E');
       document.documentElement.style.setProperty('--text-color', '#cdf5c5');
       document.documentElement.style.setProperty('--active-color', '#038C3E');
       document.documentElement.style.setProperty('--hover-color', '#77BF63');
-    }
-    else {
+    } else {
       setCurrentTheme(3);
       document.documentElement.style.setProperty('--bg-color', '#191919');
       document.documentElement.style.setProperty('--box-color', '#FFCD00');
@@ -112,22 +112,33 @@ const HomePage = () => {
       document.documentElement.style.setProperty('--active-color', '#FFCD00');
       document.documentElement.style.setProperty('--hover-color', '#b69917');
     }
-  }
-
-
+  };
 
   useEffect(
     () => setFavouriteActions(userFavouriteActions),
     [userFavouriteActions]
   );
-  useEffect(() => setBoughtActions(userBoughtActions), [userBoughtActions]);
 
+  useEffect(() => setBoughtActions(userBoughtActions), [userBoughtActions]);
+  useEffect(() => {
+    setTheme();
+    setWallpaper();
+    checkIfDataIsLoaded();
+  }, [userSettings]);
+
+  // useEffect(() => checkIfDataIsLoaded());
   return (
     <main id='home' className='home-page'>
       {restricted && <RestrictedInfo />}
       <HomePageFavourites actions={favouriteActions} />
       <HomePageDate />
       <HomePageBought actions={boughtActions} />
+      {!isLoaded && (
+        <LoadingBar
+          announcement='Trwa ładowanie danych. Proszę czekać...'
+          opacity={1}
+        />
+      )}
     </main>
   );
 };
