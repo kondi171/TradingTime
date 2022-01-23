b0955b499b321054ff3d65c12fddeda3f3c068fd
kwypych4 <kwypych4@gmail.com>
2022-01-23 18:30:39 +0100

    Added simply vaildation to modifyAccountNumber method

--- src/components/helpers/modifyAccountNumber.js
+++ src/components/helpers/modifyAccountNumber.js
@@ -1,24 +1,25 @@
 const ModifyAccountNumber = (accountNumber) => {
-  console.log(accountNumber);
-  const numberLength = accountNumber.toString().length;
-  let modifiedAccountNumber = '';
+  if (accountNumber !== undefined) {
+    const numberLength = accountNumber.toString().length;
+    let modifiedAccountNumber = '';
 
-  let j = 0;
-  for (let i = 0; i < numberLength; i++) {
-    modifiedAccountNumber += accountNumber.toString()[i];
-    if (i === 1) {
-      modifiedAccountNumber += ' ';
-      j = 0;
+    let j = 0;
+    for (let i = 0; i < numberLength; i++) {
+      modifiedAccountNumber += accountNumber.toString()[i];
+      if (i === 1) {
+        modifiedAccountNumber += ' ';
+        j = 0;
+      }
+
+      if (j === 4) {
+        modifiedAccountNumber += ' ';
+        j = 0;
+      }
+
+      j++;
     }
-
-    if (j === 4) {
-      modifiedAccountNumber += ' ';
-      j = 0;
-    }
-
-    j++;
-  }
-  return modifiedAccountNumber;
+    return modifiedAccountNumber;
+  } else return;
 };
 
 export default ModifyAccountNumber;
